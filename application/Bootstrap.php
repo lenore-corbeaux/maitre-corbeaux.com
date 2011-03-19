ac490d4345b29e145ef741fb8ff59154497e5ced
Lucas CORBEAUX <lucas.corbeaux@gmail.com>
2011-03-19 22:27:13 +0100

    Using application.ini to configure Zend_Log instead of the _initLogOptions() method in the bootstrap Closes #7

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,22 +5,6 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     /**
-     * Initialize the Zend_Log options
-     * 
-     * @return void
-     */
-    protected function _initLogOptions()
-	{
-        $this->bootstrap('log');
-        $log = $this->getResource('log');
-        $options = $this->getOptions('log');
-
-		if ($log instanceof Zend_Log && isset($options['log']['timestampFormat'])) {
-            $log->setTimestampFormat($options['log']['timestampFormat']);
-        }
-    }
-
-    /**
      * Initialize Google Analytics view helper
      * 
      * @return void
